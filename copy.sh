65a689d596ad95a96fa75dc326fd9e910a2443f4
Sam <sam.law9973@gmail.com>
2026-03-05 14:16:41 -0500

    Create copy.sh

---
+++ copy.sh
@@ -0,0 +1,16 @@
+#source folder where repo exists
+$sourceDir = ""
+
+#destination folder to put into iso 
+$destinationDir = ""
+
+#do last 30 days bc monthly updates
+$cutoff = (Get-Date).AddDays(-30)
+
+Get-ChildItem -Path $SourceDir -File -Recurse | Where-Object { $_.LastWriteTime -gt $cutoff } | ForEach-Object {
+  $relativePath = $_.FullName.Substring($sourceDir.Length)
+  $destinationPath = Join-Path -Path $destinationDir -ChildPath $relativePath
+  $destinationDirectory = Split-Path -Path $destinationPath -Parent
+}
+
+Move-Item -Path $_.FullName -Destination $destinationPath -Force
